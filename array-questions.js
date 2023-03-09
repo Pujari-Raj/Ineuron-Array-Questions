1c73e463fe24437edfe212aec74f88e133cb5e7a
Pujari-Raj <basvarajpujari607@gmail.com>
2023-03-09 23:46:26 +0530

    final changes in repo

--- array-questions.js
+++ array-questions.js
@@ -162,12 +162,37 @@ console.log('=======QUestion-13==========');
 /*13. Write a script that prompt the user to enter base and height of the triangle and 
 calculate an area of a triangle (area = 0.5 x b x h).*/
 
-// const trianglebase = prompt("Enter a base for triangle:");
-// const base = parseInt(trianglebase);
-// const triangleheight = prompt("Enter a height for triangle:");
-// const height = parseInt(triangleheight);
+const trianglebase = prompt("Enter a base for triangle:");
+const base = parseInt(trianglebase);
+const triangleheight = prompt("Enter a height for triangle:");
+const height = parseInt(triangleheight);
 
-// console.log("Area of triangle is-",(0.5*base*height));
+console.log("Area of triangle is-",(0.5*base*height));
+
+/*
+ 14. Slope Formula is m(slope) = (y2-y1)/(x2-x1). To Find the slope between point (1, 8) and point(19, 12)  
+ */
+
+ let x1 = 1;
+ let y1 = 8;
+ let x2 = 19;
+ let y2 = 12;
+ 
+ let m = (y2 - y1) / (x2 - x1); // slope
+ console.log(m);
+
+/* 15. Calculate the slope, x-intercept and y-intercept of y = 2x -2 */
+const eqn = 'y = 2x - 2'
+
+/* 16. Get radius using prompt and calculate the area of a circle (area = pi x r x r) 
+and circumference of a circle(c = 2 x pi x r) where pi = 3.14. */
+
+let radius = prompt("Enter radius of a circle:");
+
+let areaOfCircle = Math.PI * radius * radius;
+let circumOfCircle = 2 * Math.PI * radius;
+
+console.log(`Area is ${areaOfCircle}Circumference is ${circumOfCircle}`);
 
 // console.log('=======QUestion-18==========');
 
@@ -175,18 +200,142 @@ calculate an area of a triangle (area = 0.5 x b x h).*/
 feedback:'You are old enough to drive' but if not 18 give another feedback stating to 
 wait for the number of years he needs to turn 18. */
 
-// var userinput = prompt("Enter a age:");
+var userinput = prompt("Enter a age:");
 
-// if(isNaN(userinput)){
-//     alert("please provide input in numbers!!!!");
-//     return;
-// }
-// else if(userinput>=18){
-//     console.log('You are old enough to drive');
-// }
-// else{
-//     alert("wait for the "+Math.abs((userinput-18))+ "  years to turn 18");
-// }
+if(isNaN(userinput)){
+    alert("please provide input in numbers!!!!");
+    return;
+}
+else if(userinput>=18){
+    console.log('You are old enough to drive');
+}
+else{
+    alert("wait for the "+Math.abs((userinput-18))+ "  years to turn 18");
+}
+
+/* 19. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript*/
+let num1 = 24;
+if (num1 % 2 == 0) {
+    console.log(`${num1} is even`);
+} else {
+    console.log(`${num1} is odd`);
+}
+
+/* 20. Write a code which can give grades to students according to theirs scores:
+//     - 80-100, A
+//     - 70-89, B
+//     - 60-69, C
+//     - 50-59, D
+//     - 0-49, F
+*/
+
+const marks = 59;
+
+
+if (marks < 0 || marks > 100) {
+    console.log("Incorrct marks")
+} else if (marks >= 80 && marks <= 100) {
+    console.log("Grade is A")
+} else if (marks >= 70 && marks <= 79) {
+    console.log("Grade is B")
+} else if (marks >= 60 && marks <= 69) {
+    console.log("Grade is C")
+} else if (marks >= 50 && marks <= 59) {
+    console.log("Grade is D")
+} else {
+    console.log("FAIL")
+}
+
+/* 21. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// - September, October or November, the season is Autumn.
+// - December, January or February, the season is Winter.
+// - March, April or May, the season is Spring
+// - June, July or August, the season is Summer
+*/
+
+// Using Date() API of Javascript to get current month in string (i.e. September) format.
+let Month = new Date().toLocaleString('en-US', { month: 'long' });
+let season;
+if (Month === "September" || Month === "October" || Month === "November") {
+    season = "Autumn";
+} else if (Month === "December" || Month === "January" || Month === "February") {
+    season = "Winter";
+} else if (Month === "March" || Month === "April" || Month === "May") {
+    season = "Spring";
+} else if (Month === "June" || Month === "July" || Month === "August") {
+    season = "Summer";
+}
+/* 22. Write a program which tells the number of days in a month.*/
+let date = new Date();
+let month = date.getMonth() + 1;
+let days;
+
+if (month > 0 && month <= 7) {
+    if (month % 2 == 0) {
+        if (month == 2) {
+            days = "27 or 28";
+        } else {
+            days = 30;
+        }
+    } else {
+        days = 31;
+    }
+} else if (month > 7 && month <= 12) {
+    if (month % 2 == 0) {
+        days = 31;
+    } else {
+        days = 30;
+    }
+} else {
+    console.log("Invalid Month");
+}
+
+/* 23. Write a program which tells the number of days in a month, now consider leap year.*/
+
+// function to check leap year.
+function leapYear(year) {
+    if (year % 400 == 0) {
+        return true;
+    }
+    if (year % 100 == 0) {
+        return false;
+    }
+    if (year % 4 == 0) {
+        return true;
+    }
+    return false;
+}
+
+// function to find number of days.
+let getDays = (month, year) => {
+    if (month > 0 && month <= 7) {
+        if (month % 2 == 0) {
+            if (month == 2) {
+                return (leapYear(year) ? 28 : 27);
+            }
+            return 30;
+        } else {
+            return 31;
+        }
+    } else if (month > 7 && month <= 12) {
+        if (month % 2 == 0) {
+            return 31;
+        } else {
+            return 30;
+        }
+    } else {
+        console.log("Invalid Month");
+    }
+}
+
+// Driver Code.
+const getmonth = 2;
+const getyear = "2022";
+console.log(getDays(getmonth, getyear))
+
+
+console.log(`Month is ${date.toLocaleString('en-US', { month: 'long' })} and it has ${days} days.`)
+console.log(`Month is ${Month} and it is ${season} season`);
 
 /* 25 In the following shopping cart add, remove, edit items
     => const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
@@ -213,7 +362,61 @@ if(!includessugar){
 shoppingCart[3]='Green Tea';
 console.log(shoppingCart);
 
-console.log('===========QUestion-28========');
+/*
+27. The following is an array of 10 students ages:
+    => const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+    - Sort the array and find the min and max age
+    - Find the median age(one middle item or two middle items divided by two)
+    - Find the average age(all items divided by number of items)
+    - Find the range of the ages(max minus min)
+    - Compare the value of (min - average) and (max - average), use abs() method
+*/
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+// - Sort the array and find the min and max age
+const sortedArray = ages.sort()
+const min = sortedArray[0]
+let max = min;
+for (let index = 0; index < sortedArray.length; index++) {
+    max = sortedArray[index];
+}
+console.log(sortedArray)
+// - Find the median age(one middle item or two middle items divided by two)
+let median;
+if (sortedArray.length % 2 === 0) {
+    console.log('even')
+    const firstMidIndex = sortedArray.length / 2
+    const secondMidIndex = firstMidIndex - 1
+    console.log(firstMidIndex)
+    console.log(secondMidIndex)
+    const firstMidItem = sortedArray[firstMidIndex]
+    const secondMidItem = sortedArray[secondMidIndex]
+    median = (firstMidItem + secondMidItem) / 2
+} else {
+    console.log('odd')
+    const midIndex = Math.floor(sortedArray.length / 2)
+    median = sortedArray[midIndex] / 2
+}
+console.log(median)
+
+// Find the range of the ages(max minus min)
+const range = max - min
+
+// - Find the average age(all items divided by number of items)
+let sum = min
+for (let index = 0; index < sortedArray.length; index++) {
+    const element = sortedArray[index];
+    sum = sum + element
+}
+const avg = sum / sortedArray.length
+
+// - Compare the value of (min - average) and (max - average), use abs() method
+const a = Math.abs(min - avg)
+const b = Math.abs(max - avg)
+
+const compare = Math.abs(a - b)
+
 /*28. Use for loop to iterate from 0 to 100 and print only prime numbers*/ 
 
 function isPrime(value) {
@@ -239,6 +442,29 @@ for(var i=1; i<=num;i++){
         console.log(i);
     }
 }
+
+/* 29. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.*/
+
+let evenSum = 0;
+let oddSum = 0;
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        evenSum = evenSum + i;
+    } else {
+        oddSum = oddSum + i;
+    }
+}
+
+console.log(`
+Even Sum is ${evenSum}
+Odd Sum is ${oddSum}
+`);
+
+// 30. Write a script which generates a random hexadecimal number.
+
+let num = Math.random() * 16;
+console.log(Math.round(num));
 
 /*31*/
 const countryList = [
@@ -496,7 +722,16 @@ const countryList = [
 //console.log(countryList.sort());
 
 
-/*32 */
+/*32 
+//  Array Questions
+// - Extract all the countries contain the word 'land' from the countries array and print it as array
+// - Find the country containing the hightest number of characters in the countries array
+// - Extract all the countries contain the word 'land' from the countries array and print it as array
+// - Extract all the countries containing only four characters from the countries array and print it as array
+// - Extract all the countries containing two or more words from the countries array and print it as array
+// - Reverse the countries array and capitalize each country and stored it as an array
+
+*/
 
 let res = countryList.filter((country) => country.includes('land'));
 console.log(res);
@@ -519,3 +754,18 @@ for(var i = 0; i<countryList.length; i++){
         myarray.push(i);
     }
 }
+
+/* 35. Write a functions which checks if all items are unique in the array.*/
+
+let arr = [22, 32, 43, "Pujari", "Basvaraj", 43, false, true];
+let uniqueArray = [];
+let notUniqueArray = [];
+arr.forEach((num) => {
+    if (arr.indexOf(num) === arr.lastIndexOf(num)) {
+        uniqueArray.push(num);
+    } else {
+        notUniqueArray.push(num);
+    }
+})
+console.log(uniqueArray);
+console.log(notUniqueArray);
